a293e5d5401dd625c64cbac0fa6a3897215cdae7
Pavle Josipovic <pjosipovic@tenstorrent.com>
2026-08-23 19:09:40 +0000

    Migrate PaddedSlice to Metalium 2.0

--- ttnn/cpp/ttnn/operations/experimental/quasar/padded_slice/device/padded_slice_tile_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/experimental/quasar/padded_slice/device/padded_slice_tile_program_factory.cpp
@@ -485,7 +485,7 @@ PaddedSliceTileProgramFactory::cached_program_t PaddedSliceTileProgramFactory::c
     };
 
     const std::string compute_kernel =
-        "ttnn/cpp/ttnn/operations/sliding_window/halo/device/kernels/compute/pack_untilize.cpp";
+        "ttnn/cpp/ttnn/operations/experimental/quasar/halo/device/kernels/compute/pack_untilize.cpp";
 
     auto untilize_compute_kernel_id = CreateKernel(
         program, compute_kernel, total_cores, ComputeConfig{.fp32_dest_acc_en = false, .compile_args = compute_args});
@@ -503,14 +503,14 @@ PaddedSliceTileProgramFactory::cached_program_t PaddedSliceTileProgramFactory::c
     tt::tt_metal::TensorAccessorArgs(src0_buffer).append_to(reader_compile_time_args_vec);
     tt::tt_metal::KernelHandle unary_reader_kernel_id = tt::tt_metal::CreateKernel(
         program,
-        "ttnn/cpp/ttnn/operations/experimental/padded_slice/device/kernels/dataflow/"
+        "ttnn/cpp/ttnn/operations/experimental/quasar/padded_slice/device/kernels/dataflow/"
         "padded_slice_reader_tiled_interleaved_start_id.cpp",
         total_cores,
         tt::tt_metal::ReaderDataMovementConfig(reader_compile_time_args_vec));
 
     tt::tt_metal::KernelHandle unary_writer_kernel_id = tt::tt_metal::CreateKernel(
         program,
-        "ttnn/cpp/ttnn/operations/experimental/padded_slice/device/kernels/dataflow/"
+        "ttnn/cpp/ttnn/operations/experimental/quasar/padded_slice/device/kernels/dataflow/"
         "writer_unary_sharded_padded_tiled.cpp",
         total_cores,
         tt::tt_metal::WriterDataMovementConfig(writer_compile_time_args_vec));
